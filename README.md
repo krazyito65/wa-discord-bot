a1331e903e3aadd1a42d12ded51f5713a4ed5e85
Leon Solis <Leon.Solis@wnco.com>
2018-02-23 00:19:26 -0600

    Had to move new release webhook stuff to a new file to run seperatly

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ This is a very work in progress bot that will allow for git pull requests from t
 First thing you should do is generate a token file to put in the root folder of your bot. It should simply be called `token` and it contains the token from the application you create here: https://discordapp.com/developers/applications/me
 
 Then use: `npm install` to install the dependencies for this bot, then run using `node bot.js`
+`release_hook.js` is a seperate file to run with node (use forever to run 2 items easiy)
 
 The commands folder contains all the available commands.
---
+++ release_hook.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var request = require('request');
+var hook = fs.readFileSync('hook', "utf8").toString().trim();
+
+const server = http.createServer();
+
+server.on('request', (req, response) => {
+  // we should only get POSTS, so ignore everything else.
+  response.writeHead(200,{"Content-Type":"application/json"});
+  if (req.method == "POST") {
+    log("Got a post request. Doing stuff..")
+    var git;
+    var payload;
+    req.on('data', (chunk) => {
+      git = JSON.parse(chunk)
+      log(git.release.body)
+      payload = {
+        "username": "WeakAuras-Release",
+        "avatar_url": "https://media.forgecdn.net/avatars/62/782/636142194921799650.png",
+        "embeds": [{
+          "title": git.release.tag_name,
+          "description": git.release.body,
+          "url": git.release.url,
+          "color": 1399932,
+          "timestamp": moment().format(),
+          // "fields": [
+          //   {
+          //     "name": "Links",
+          //     "value": "[Request this webhook](https://goo.gl/forms/S1mkG70XDU543KO23) | [Github](https://github.com/krazyito65/mmo-champion-rss-webhook)"
+          //   }
+          // ],
+          // "footer": {
+          //   "text": "This webook is in alpha. Contact Krazyito#1696"
+          // },
+          // "thumbnail": {
+          //   "url": THUMBNAIL_URL//"http://static.mmo-champion.com/images/tranquilizing/logo.png"
+          // },
+        }]
+      }
+      request({
+        url: hook,
+        method: "POST",
+        body: payload,
+        json: true
+      });
+    }).on('end', () => {
+      response.end("I got your response.");
+    });
+  }
+  else {
+    response.end("Undefined request .");
+  }
+});
+
+server.listen(8000);
+console.log("Server running on port 8000");
