b9865cb86f9dd81e604bf4555e641f6cb9c21269
Leon Solis <Leon.Solis@wnco.com>
2018-02-27 14:34:28 -0600

    only allowing people with manage msg permisson to remove quotes

--- commands/quote.js
+++ commands/quote.js
@@ -5,20 +5,27 @@ var quotes = new JsonDB("./data/quotes", true, true);
 
 module.exports = function (args, user, userID, channelID, bot){
     var serverID = bot.channels[channelID].guild_id
+    var isMod = canManageMessages(bot, userID, serverID)
+
     try { quotes.getData("/"+serverID) }
     catch(error) { quotes.push("/"+serverID, []) }
+
     var serverQuotes = quotes.getData("/"+serverID)
+
     if (!args){
         var item = serverQuotes[Math.floor(Math.random()*serverQuotes.length)];
         botFuncs.sendMsg(channelID, item)
         return
     }
+
     args = args.split(" ")
     var option = args[0]
     args.shift();
     args = args.join(" ");
+
     var isValidOption = /(add|\+)/.test(option)
     var isRemove = /(remove|-)/.test(option)
+
     if  (!isValidOption && !isRemove) {
         botFuncs.sendMsg(channelID, "Not a valid option\nUse !quote [add|+|remove|-] quote")
         return
@@ -31,6 +38,10 @@ module.exports = function (args, user, userID, channelID, bot){
         return
     }
     if (isRemove) {
+        if (!isMod) {
+            botFuncs.sendMsg(channelID, "You're not a mod. Denied.")
+            return
+        }
         serverQuotes = removeFrmArr(serverQuotes, args)
         if (serverQuotes === 0) {
             botFuncs.sendMsg(channelID, "Quote not found.")
@@ -45,4 +56,11 @@ module.exports = function (args, user, userID, channelID, bot){
 function removeFrmArr(array, element) {
   if (!array.includes(element)) { return 0 }
   return array.filter(e => e !== element);
-};
+}
+
+//https://github.com/izy521/discord.io/blob/master/docs/permissions.md
+function canManageMessages(client, userID, serverID) {
+    var server = client.servers[serverID];
+    var member = server.members[userID];
+    return member.roles.some( roleID => server.roles[roleID].TEXT_MANAGE_MESSAGES );
+}
